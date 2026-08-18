34de341cdfe33105dd9dc0bb5f8eafb1eadd3640
CloudLSR <bernardbombsquad@gmail.com>
2026-08-17 21:55:29 -0500

    Feat: Add Correct Freeplay Select Sprites
    Replaced the placeholder text based options with the Redux/Classic sprites using their correct idle and selected animations, added a pink/blue background color transition per selection, and fixed vertical centering so each logo scales without overlapping the other.

--- source/states/FreeplaySelect.hx
+++ source/states/FreeplaySelect.hx
@@ -1,17 +1,27 @@
 package states;
 
 import flixel.FlxSprite;
-import flixel.text.FlxText;
 
 class FreeplaySelect extends MusicBeatState
 {
 	public static var lastFreeplayWasLegacy:Bool = false;
 
-	var options:Array<String> = ['Redux Freeplay', 'Legacy Freeplay'];
-	var optionsText:Array<Alphabet> = [];
+	var spriteNames:Array<String> = ['FreeplayRedux', 'FreeplayClassic'];
+	var animPrefixes:Array<String> = ['redux', 'classic'];
+
+	var options:Array<FlxSprite> = [];
+	var slotCenterY:Array<Float> = [];
 	var curSelected:Int = 0;
 
+	var idleScale:Array<Float> = [1, 1];
+	var selectedScale:Array<Float> = [1, 1];
+
 	var bg:FlxSprite;
+
+	var reduxColor:FlxColor = 0xFFEAA8FF;
+	var legacyColor:FlxColor = 0xFF665AFF;
+	var intendedColor:FlxColor;
+	var colorTween:FlxTween;
 
 	override function create()
 	{
@@ -21,20 +31,42 @@ class FreeplaySelect extends MusicBeatState
 		DiscordClient.changePresence("In the Menus", null);
 		#end
 
+		curSelected = lastFreeplayWasLegacy ? 1 : 0;
+		intendedColor = (curSelected == 1) ? legacyColor : reduxColor;
+
 		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.antialiasing = ClientPrefs.data.antialiasing;
-		bg.color = 0xFF665AFF;
+		bg.color = intendedColor;
 		add(bg);
 		bg.screenCenter();
 
-		curSelected = lastFreeplayWasLegacy ? 1 : 0;
-
-		for (i in 0...options.length)
+		for (i in 0...spriteNames.length)
 		{
-			var optText:Alphabet = new Alphabet(0, 280 + (i * 120), options[i], true);
-			optText.screenCenter(X);
-			optionsText.push(optText);
-			add(optText);
+			var opt:FlxSprite = new FlxSprite();
+			opt.antialiasing = ClientPrefs.data.antialiasing;
+			opt.frames = Paths.getSparrowAtlas('mainmenu/' + spriteNames[i]);
+			opt.animation.addByPrefix('idle', animPrefixes[i] + ' white', 24);
+			opt.animation.addByPrefix('selected', animPrefixes[i] + ' basic', 24);
+			opt.animation.play('selected');
+			opt.scale.set(selectedScale[i], selectedScale[i]);
+			opt.updateHitbox();
+			opt.ID = i;
+			options.push(opt);
+		}
+
+		var padding:Float = 30;
+		var totalHeight:Float = padding * (options.length - 1);
+		for (opt in options)
+			totalHeight += opt.height;
+
+		var yPos:Float = (FlxG.height - totalHeight) / 2;
+		for (opt in options)
+		{
+			opt.y = yPos;
+			opt.screenCenter(X);
+			add(opt);
+			slotCenterY.push(yPos + opt.height / 2);
+			yPos += opt.height + padding;
 		}
 
 		updateOptions();
@@ -72,15 +104,41 @@ class FreeplaySelect extends MusicBeatState
 			}
 		}
 
+		for (opt in options)
+			opt.screenCenter(X);
+
 		super.update(elapsed);
 	}
 
 	function updateOptions()
 	{
-		for (i in 0...optionsText.length)
+		for (i in 0...options.length)
 		{
+			var opt:FlxSprite = options[i];
 			var selected:Bool = (i == curSelected);
-			optionsText[i].alpha = selected ? 1 : 0.6;
+
+			opt.animation.play(selected ? 'selected' : 'idle');
+			var s:Float = selected ? selectedScale[i] : idleScale[i];
+			opt.scale.set(s, s);
+			opt.updateHitbox();
+			opt.centerOffsets();
+			opt.y = slotCenterY[i] - (opt.height / 2);
+			opt.alpha = 1; // full alpha - visibilidad
+		}
+
+		var newColor:FlxColor = (curSelected == 1) ? legacyColor : reduxColor;
+		if (newColor != intendedColor)
+		{
+			if (colorTween != null)
+				colorTween.cancel();
+
+			intendedColor = newColor;
+			colorTween = FlxTween.color(bg, 0.4, bg.color, intendedColor, {
+				onComplete: function(twn:FlxTween)
+				{
+					colorTween = null;
+				}
+			});
 		}
 	}
 }
